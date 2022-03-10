0f0076fa5c7baaee7a50909e78e344c22b44572e
UP2013168 <91673755+UP2013168@users.noreply.github.com>
2022-03-10 19:12:50 +0000

    Added keyboard, made ui a bit cleaner

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,19 +1,22 @@
-window.onload = (event) => {
-  console.log('page is fully loaded');
-  createTiles();
-};
+const keys = [
+  'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+   'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+  '↵','Z', 'X', 'C', 'V', 'B', 'N', 'M', '←'
+]
+
+
 
 function createTiles(){
   const board = document.querySelector('#board');
   const tile = document.createElement('div');
   const row = document.createElement('div');
-  row.id = 'board-row';
-  tile.id = 'tile';
+  row.classList.add('board-row');
+  tile.classList.add('tile');
   // Create rows
   for (let i = 0; i < 6; i++){
     board.append(row.cloneNode(true));
   }
-  const rows = document.querySelectorAll('#board-row');
+  const rows = document.querySelectorAll('.board-row');
   // Add 5 tiles to each row
   for (let row1 of rows){
     for (let i = 0; i < 5; i++){
@@ -21,3 +24,35 @@ function createTiles(){
     }
   }
 }
+
+function createKeyboard(){
+  const keyRows = document.querySelectorAll('.key-row')
+  const keyButton = document.createElement('button');
+  for (const key of keys){
+    let newKey = keyButton.cloneNode(true);
+    newKey.classList.add('key');
+    newKey.textContent = key;
+    // Top row of letters
+    if(keys.indexOf(key)<10){
+      keyRows[0].append(newKey);
+    }
+    // Middle row of letters
+    else if(keys.indexOf(key)<19){
+      keyRows[1].append(newKey);
+    }
+    // Bottom row of letters
+    else{
+      keyRows[2].append(newKey);
+    }
+    // Make backspace and enter larger
+    if (keys.indexOf(key) == 19 || keys.indexOf(key) == 27){
+      newKey.classList.add("larger");
+    }
+  }
+}
+
+window.onload = (event) => {
+  createTiles();
+  createKeyboard();
+  console.log('page is fully loaded');
+};
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
+app.use(express.static('public'));
 
+/*
 app.get("/", (req, res) =>{
-    app.use(express.static('public'))
     res.sendFile('public/index.html', {root: __dirname })
     // here instead you can do res.send("text to render")
 })
+*/
 
 app.listen(port, () => {
     console.log("App listening at http://localhost:${port}");
