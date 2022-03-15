0ae5c3bce2a394a28cd3d718906e0327f03de33c
UP2013168 <91673755+UP2013168@users.noreply.github.com>
2022-03-15 16:02:31 +0000

    Redoing javascript

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,80 +1,80 @@
-const keys = [
-  'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
-   'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
-  '↵','Z', 'X', 'C', 'V', 'B', 'N', 'M', '←'
-]
-
-
-
-function createTiles(){
-  const board = document.querySelector('#board');
-  const tile = document.createElement('div');
-  const row = document.createElement('div');
-  row.classList.add('board-row');
-  tile.classList.add('tile');
-  // Create 6 rows
-  for (let i = 0; i < 6; i++){
-    board.append(row.cloneNode(true));
-  }
-  const rows = document.querySelectorAll('.board-row');
-  // Add 5 tiles to each row
-  for (let row1 of rows){
-    row1.setAttribute("letters", "");
-    for (let i = 0; i < 5; i++){
-      row1.append(tile.cloneNode(true));
-    }
-  }
-}
-
-function createKeyboard(){
-  const keyRows = document.querySelectorAll('.key-row')
-  const keyButton = document.createElement('button');
-  for (const key of keys){
-    let newKey = keyButton.cloneNode(true);
-    newKey.classList.add('key');
-    newKey.textContent = key;
-    // Top row of letters
-    if(keys.indexOf(key)<10){
-      keyRows[0].append(newKey);
-    }
-    // Middle row of letters
-    else if(keys.indexOf(key)<19){
-      keyRows[1].append(newKey);
-    }
-    // Bottom row of letters
-    else{
-      keyRows[2].append(newKey);
-    }
-    // Make backspace and enter larger
-    if (keys.indexOf(key) == 19 || keys.indexOf(key) == 27){
-      newKey.classList.add("larger");
-    }
-  }
-}
-
-
-function typingHandler(typed){
-  const letter = typed.key;
-  const rows = document.querySelectorAll('.board-row');
-  const row = rows[0];
-  const tiles = row.children;
-  let wordLength = Object.keys(row.getAttribute("letters")).length;
-    if (letter.match(/^[a-z]$/)){
-      if (wordLength < 5){
-        row.setAttribute("letters", row.getAttribute('letters') + letter);
-        tiles[0].textContent = letter;  
-      }
-      
-    else if (letter == 'Backspace'){
-        console.log("backspace");
-        row.setAttribute("letters", row.getAttribute("letters").slice(0, wordLength-1));
-      }
-  }
-}
-
-window.onload = (event) => {
+document.addEventListener("DOMContentLoaded",() =>{
   createTiles();
   createKeyboard();
-  window.addEventListener("keydown", typingHandler);
-  console.log('page is fully loaded');
-};
+
+  let guessedWords = [[]];
+  let availableSpace = 1;
+
+  // Add event listeners to each key on the keyboard
+  const keys = document.querySelectorAll('.key-row button');
+
+  for (let key of keys){
+    key.onclick = ({target}) => {
+      const letter = target.getAttribute('data-key');
+
+      updateGuessedWords(letter);
+    }
+  }
+
+    function getCurrentWordArr() {
+      const numberOfGuessedWords = guessedWords.length;
+      return guessedWords[numberOfGuessedWords -1];
+    }
+
+    function updateGuessedWords(letter){
+      const currentWordArr = getCurrentWordArr();
+      if (currentWordArr && currentWordArr.length < 5) {
+        currentWordArr.push(letter);
+
+        const availableSpaceEl = document.getElementById(String(1));
+        availableSpace += 1;
+
+        availableSpaceEl.textContent = letter;
+      }
+    }
+
+    function createTiles(){
+      const board = document.querySelector('#board');
+      
+        for (let i=0; i < 30; i++){
+          let tile = document.createElement("div");
+          tile.classList.add("tile");
+          tile.setAttribute("id", i+1);
+          board.append(tile);
+        }
+      }
+
+    function createKeyboard(){
+      const keyLetters = [
+        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+         'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+        'enter','z', 'x', 'c', 'v', 'b', 'n', 'm', 'del'
+      ]
+
+      for (const key of keyLetters){
+        let keyRows = document.querySelectorAll('.key-row')
+        let newKey = document.createElement('button');
+
+        newKey.setAttribute('data-key', key);
+        newKey.textContent = key;
+
+        // Top row of letters
+        if(keyLetters.indexOf(key)<10){
+          keyRows[0].append(newKey);
+        }
+        // Middle row of letters
+        else if(keyLetters.indexOf(key)<19){
+          keyRows[1].append(newKey);
+        }
+        // Bottom row of letters
+        else{
+          keyRows[2].append(newKey);
+        }
+        // Make backspace and enter larger
+        if (keyLetters.indexOf(key) == 19 || keyLetters.indexOf(key) == 27){
+          newKey.classList.add("larger");
+        }
+      }
+    }
+
+  })
