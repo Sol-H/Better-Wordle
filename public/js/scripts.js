dc0f32ee6687c0c6b88ce7ab8cc5b6a49c98447f
UP2013168 <91673755+UP2013168@users.noreply.github.com>
2022-05-13 18:55:29 +0100

    Changed some colours

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,29 +2,31 @@ document.addEventListener('DOMContentLoaded', () => {
   createTiles();
   createKeyboard();
 
+  // Value updated depending on the gameOver cookie; makes sure localStorage is only written to once
   let gameOver;
+  // Used to make the emojiScore; updated with each word guess.
   let score = '';
-  const guessedWords = [[]]; // Stores the users guessed words
-  let availableSpace = 1; // Used to determine where the most recent letter was typed
-
+  // Stores the users guessed words
+  const guessedWords = [[]];
+  // Used to determine where the most recent letter was typed
+  let currentTileId = 1;
   let guessedWordCount = 0;
 
   // Creates a string for midnight the day after the current date, used for cookies
   const date = new Date();
-  const tomorrow = new Date(date);// take away an hour because we are in BST
-  // tomorrow.setDate(date.getDate() + 1);
-  let tomorrowstr = tomorrow.toString().split(' ');
-  tomorrowstr[4] = '23:00:00';
-  tomorrowstr.splice(6, 3); // Remove the last parts of the string to fit the standard for cookies
-  tomorrowstr = tomorrowstr.join(' ');
-  
-  // Toast stuff
+  // Make array of date
+  let tomorrow = date.toString().split(' ');
+  // Set the time to 11pm of current date (This is because we are 1 hour ahead due to summer time)/
+  tomorrow[4] = '23:00:00';
+  // Remove the last parts of the string to fit the standard for cookies
+  tomorrow.splice(6, 3);
+  tomorrow = tomorrow.join(' ');
 
+  // Toast elements (Toast is the notifications that pop up)
   const toast = document.querySelector('.toast');
-  const toastContent = document.querySelector('.toast-content');
   const toastMessage = document.querySelector('#toastMessage');
 
-  // Stats Modal stuff
+  // Stats Modal elements
   const statsModal = document.querySelector('#statsModal');
   const statsClose = document.querySelector('#statsClose');
   const statsBtn = document.querySelector('#stats');
@@ -33,55 +35,71 @@ document.addEventListener('DOMContentLoaded', () => {
   const shareBtn = document.querySelector('#shareButton');
 
   statsBtn.onclick = function () {
+    // Show the modal when the stats button is clicked.
     statsModal.style.display = 'block';
   };
 
   statsClose.onclick = function () {
+    // Hide the modal when the close button is clicked.
     statsModal.style.display = 'none';
   };
 
   window.onclick = function (event) {
+    // Hide the modal if clicked behind it.
     if (event.target === statsModal) {
       statsModal.style.display = 'none';
     }
   };
 
   shareBtn.onclick = function () {
+    // Copies the emojis for the score to the users clipboard.
     copyScore();
     console.log('copied');
   };
 
-  // INfo Modal stuff
+  // Info modal elements.
   const infoModal = document.querySelector('#infoModal');
   const infoClose = document.querySelector('#infoClose');
   const infoBtn = document.querySelector('#info');
 
-  loadFromCookies();
-
   infoBtn.onclick = function () {
+    // Show info modal when button clicked
     infoModal.style.display = 'block';
   };
 
   infoClose.onclick = function () {
+    // Hide modal when close button clicked
     infoModal.style.display = 'none';
   };
 
   window.onclick = function (event) {
     if (event.target === infoModal) {
+      // Hide modal when clicked behind.
       infoModal.style.display = 'none';
     }
   };
 
-
+  // If there are no scores in the local storage, set all values to 0.
   if (!localStorage.getItem('scores')) {
     localStorage.setItem('scores', JSON.stringify({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, fail: 0 }));
   }
+  // If there are no streak in the local storage, set to 0.
+  if (!localStorage.getItem('streak')) {
+    localStorage.setItem('streak', JSON.stringify({ amount: 0 }));
+  }
 
+  // Iterates through cookies, and loads the typed words, gameOver and info into the game.
+  loadFromCookies();
+
+  // Loads the stats from localStorage, then displays them in the statistics modal.
   loadStats();
 
+  // Gets alements for keyboard
   const keys = document.querySelectorAll('.key-row button');
 
+  // Makes all the keyboard buttons clickable.
   addKeyboardClicks();
+  // Makes user able to type on physical keyboard
   window.addEventListener('keydown', typingHandler);
 
   // Add functionality for typing on physical keyboard
@@ -89,17 +107,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const letter = event.key;
 
     if (letter === 'Enter' && !gameOver) {
+      // Checks contents of the currentWordArr and make decisions based on it. (If its a real word etc)
       submitWord();
       return;
     }
 
     if (letter === 'Backspace' && !gameOver) {
+      // Removes letter from the tile and makes the currentTile id less
       deleteLetter();
       return;
     }
 
-    // Make sure only letters are allowed
+    // Make sure only letters are allowed.
     if (letter.match(/^[a-z]$/)) {
+      // Append letter to the current word array.
       updateGuessedWords(letter);
     }
   }
@@ -172,7 +193,8 @@ document.addEventListener('DOMContentLoaded', () => {
           case 'absent': break; // If absent keep tile grey
         }
 
-        letterEl.classList.add('animate__flipInX');
+        // Make tile flip when colour is shown
+        letterEl.classList.add('flip-tile');
         letterEl.style = `outline-color:${tileColor};background-color:${tileColor};`;
 
         // Change the colors of keyboard buttons based on guess
@@ -187,19 +209,25 @@ document.addEventListener('DOMContentLoaded', () => {
     // The rest of this if statement, loads in the stats.
     if (String(results) === 'correct,correct,correct,correct,correct') {
       // Add cookie for the final word
-      document.cookie = `word${guessedWords.length}=${currentWord}; expires=${tomorrowstr}; path=/;`;
+      document.cookie = `word${guessedWords.length}=${currentWord}; expires=${tomorrow}; path=/;`;
       // Add cookie for the game over status, so scores cant be saved twice
-      document.cookie = `gameOver=true; expires=${tomorrowstr}; path=/;`;
+      document.cookie = `gameOver=true; expires=${tomorrow}; path=/;`;
       // Makes stats modal visible
       statsModal.style.display = 'block';
 
       if (!gameOver) {
         const scores = JSON.parse(localStorage.getItem('scores'));
+        const streak = JSON.parse(localStorage.getItem('streak'));
+        streak.amount = streak.amount + 1;
         scores[guessedWordCount] = scores[guessedWordCount] + 1;
         localStorage.setItem('scores', JSON.stringify(scores));
+        localStorage.setItem('streak', JSON.stringify(streak));
       }
 
       statsContentScore.textContent = `Soldle Score: ${guessedWordCount}/6`;
+
+      // Display share button.
+      shareBtn.style.display = 'block';
 
       Toast('You win!');
       currentWord = '';
@@ -210,22 +238,26 @@ document.addEventListener('DOMContentLoaded', () => {
     if (guessedWords.length === 6) {
       Toast('No more guesses');
       // Add cookie for the final word
-      document.cookie = `word${guessedWords.length}=${currentWord}; expires=${tomorrowstr}; path=/;`;
+      document.cookie = `word${guessedWords.length}=${currentWord}; expires=${tomorrow}; path=/;`;
       // Add a fail to the stats in local storage
       const scores = JSON.parse(localStorage.getItem('scores'));
-      scores.fail = scores[guessedWordCount] + 1;
+      scores.fail = scores.fail + 1;
       localStorage.setItem('scores', JSON.stringify(scores));
+      localStorage.setItem('streak', JSON.stringify({ amount: 0 }));
       statsModal.style.display = 'block';
       gameOver = true;
       guessedWordCount = 'X';
       statsContentScore.textContent = `Soldle Score: ${guessedWordCount}/6`;
+
+      // Display share button.
+      shareBtn.style.display = 'block';
       return;
     }
 
     const guessedLen = guessedWords.push([]);
 
     // Creates cookie so that data is saved until the next day
-    document.cookie = `word${guessedLen - 1}=${currentWord}; expires=${tomorrowstr}; path=/;`;
+    document.cookie = `word${guessedLen - 1}=${currentWord}; expires=${tomorrow}; path=/;`;
   }
 
   async function checkRealWord(word_) {
@@ -241,11 +273,12 @@ document.addEventListener('DOMContentLoaded', () => {
       currentWordArr.pop();
       guessedWords[guessedWords.length - 1] = currentWordArr;
 
-      const lastLetterEl = document.getElementById(String(availableSpace - 1));
+      const currentTile = document.getElementById(String(currentTileId - 1));
 
-      lastLetterEl.textContent = '';
-      lastLetterEl.style = 'outline-color:#272729';
-      availableSpace = availableSpace - 1;
+      currentTile.textContent = '';
+      currentTile.style = 'outline-color: #565758;';
+      console.log('hi');
+      currentTileId = currentTileId - 1;
     }
   }
 
@@ -255,24 +288,29 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function updateGuessedWords(letter) {
+    // Array of the current word being typed.
     const currentWordArr = getCurrentWordArr();
     if (currentWordArr && currentWordArr.length < 5) {
+      // Pushes letter being typed into the currentWordArr.
       currentWordArr.push(letter);
-      const availableSpaceEl = document.getElementById(String(availableSpace));
-      availableSpace += 1;
-      availableSpaceEl.style = 'outline-color:rgb(58,58,60)';
-      availableSpaceEl.textContent = letter;
+      // Uses availableSpace variable to get the tile element.
+      const currentTile = document.getElementById(String(currentTileId));
+      // Increments available space so that the next tile can be used next time
+      currentTileId += 1;
+      // Makes outline colour a different shade to show tile is typed.
+      currentTile.style = 'outline-color:rgb(58,58,60)';
+      // Put the current typed letter into the tile.
+      currentTile.textContent = letter;
     }
   }
 
-
+  // Creates a 5x6 grid of tiles.
   function createTiles() {
     const board = document.querySelector('#board');
 
     for (let i = 0; i < 30; i++) {
       const tile = document.createElement('div');
       tile.classList.add('tile');
-      tile.classList.add('animate__animated');
       tile.setAttribute('id', i + 1);
       board.append(tile);
     }
@@ -282,7 +320,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const keyLetters = [
       'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
       'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-      'enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'del',
+      'enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'delete',
     ];
 
     for (const key of keyLetters) {
@@ -331,17 +369,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // This function loads the stats from localStorage, then displays them in the statistics modal.
   function loadStats() {
-    const stats = JSON.parse(localStorage.getItem('scores'));
-    const played = getTimesPlayed(stats);
+    // Get stats from localStorage
+    const scores = JSON.parse(localStorage.getItem('scores'));
+    const winStreak = JSON.parse(localStorage.getItem('streak'));
+    const played = getTimesPlayed(scores);
+    const winPercent = getWinPercent(scores);
+
+
+    // Make elements for stats
+    const streakElem = document.createElement('h3');
     const playedElem = document.createElement('h3');
-    playedElem.textContent = `Games Played, ${played}.`;
-
+    const winPercentElem = document.createElement('h3');
     const guessDist = document.createElement('p');
-    guessDist.textContent = 'Guess Distribution:';
+    playedElem.textContent = `Games played, ${played}.`;
+    streakElem.textContent = `Current win streak, ${winStreak.amount}.`;
+    winPercentElem.textContent = `Win percentage, ${winPercent}%.`;
+    guessDist.textContent = 'Guess distribution,';
 
+    // Append the stats to the stats modal.
     statsContent.append(playedElem);
+    statsContent.append(streakElem);
+    statsContent.append(winPercentElem);
     statsContent.append(guessDist);
-    shareBtn.style.display = 'block';
 
     for (let i = 1; i < 8; i++) {
       // Create an SVG element for the bar chart
@@ -349,9 +398,9 @@ document.addEventListener('DOMContentLoaded', () => {
       const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
       const lineBreak = document.createElement('br');
       // Get the number for the current stat (will affect length of barchart)
-      let stat = JSON.stringify(stats[`${i}`]);
+      let stat = JSON.stringify(scores[`${i}`]);
       if (!stat) {
-        stat = JSON.stringify(stats.fail);
+        stat = JSON.stringify(scores.fail);
       }
       svg.setAttribute('height', '30px');
       svg.setAttribute('width', `${stat * 10}px`);
@@ -375,27 +424,38 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  function getTimesPlayed(stats) {
+  function getTimesPlayed(scores) {
     let times = 0;
     // Adds all the values of the stats together, to get the amoutn of times played
-    for (const [, stat] of Object.entries(stats)) {
+    for (const [, stat] of Object.entries(scores)) {
       times += stat;
     }
     return times;
+  }
+
+  function getWinPercent(scores) {
+    let wins = 0;
+    let losses = 0;
+    // Adds all the values of the wins together, and all losses together
+    for (const [score, stat] of Object.entries(scores)) {
+      if (score === 'fail') {
+        losses += stat;
+      } else {
+        wins += stat;
+      }
+    }
+    console.log(wins, losses);
+    // Calculate percentage rounded to 1 decimal place
+    return Math.round(wins / (wins + losses) * 1000) / 10;
   }
 
   function Toast(message) {
     toast.style.display = 'block';
     toastMessage.textContent = message; // Show toast
     setTimeout(() => {
-      toastContent.classList.add('animate__fadeOutUp'); // Adds animation to show toast fading away
-    }, 1000);
-    setTimeout(() => {
       toast.style.display = 'none';
-      toastContent.classList.remove('animate__fadeOutUp');
-    }, 1200);
+    }, 1500);
   }
-
 
   async function loadFromCookies() {
     const cookies = document.cookie;
@@ -413,7 +473,7 @@ document.addEventListener('DOMContentLoaded', () => {
       } // Info modal
       if (!cookies.includes('info=read')) {
         infoModal.style.display = 'block';
-        document.cookie = `info=read;expires=${tomorrowstr}; path=/;`;
+        document.cookie = `info=read;expires=${tomorrow}; path=/;`;
       }
       if (!cookies.includes('gameOver=true')) {
         gameOver = false;
