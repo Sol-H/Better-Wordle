85f9f0b0049195a6c36b7a346430bb5d4f43cf0d
UP2013168 <91673755+UP2013168@users.noreply.github.com>
2023-04-04 13:42:37 +0100

    Made compatible for vercel

--- server.mjs
+++ server.mjs
@@ -5,6 +5,9 @@ import * as gameHandler from './gameHandler.mjs';
 const app = express();
 
 app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
